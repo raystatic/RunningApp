07bf4ed676d2ee933eb5e59177db45269fc6e42b
raystatic <rahul9650ray@gmail.com>
2020-08-18 02:11:13 +0530

    showing runs in reclerview

--- app/src/main/java/com/running/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/running/runningapp/ui/fragments/RunFragment.kt
@@ -6,8 +6,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.running.runningapp.R
+import com.running.runningapp.adapters.RunAdapter
 import com.running.runningapp.other.Constants.REQUEST_CODE_PERMISSION_LOCATION
 import com.running.runningapp.other.TrackingUtility
 import com.running.runningapp.ui.viewmodels.MainViewModel
@@ -21,12 +24,26 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
 
     private val viewModel : MainViewModel by viewModels()
 
+    private lateinit var runAdapter: RunAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requestPermissions()
+        setUpRecyclerView()
+
+        viewModel.runSortedByDate.observe(viewLifecycleOwner, Observer {
+            runAdapter.submitList(it)
+        })
+
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment2_to_trackingFragment2)
         }
+    }
+
+    private fun setUpRecyclerView() = rvRuns.apply {
+        runAdapter = RunAdapter()
+        adapter = runAdapter
+        layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun requestPermissions(){
